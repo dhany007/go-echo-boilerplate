f73d8c19d3ecb410bb372cda78a44fe223392e27
dhanykoinworks <dhany.aritonang@koinworks.com>
2023-05-22 11:35:54 +0700

    add echo and ping route

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
 
 func main() {
-	fmt.Println("go echo framework")
+	e := echo.New()
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, response{
+			Status:  "success",
+			Message: "pong",
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
