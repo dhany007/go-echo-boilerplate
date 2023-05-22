6ef743153d04c7f9996d0d06850f5b468091cbd5
dhanykoinworks <dhany.aritonang@koinworks.com>
2023-05-22 13:34:30 +0700

    add todo:

--- main.go
+++ main.go
@@ -11,15 +11,43 @@ type response struct {
 	Message string `json:"message"`
 }
 
+func ping(c echo.Context) error {
+	return c.JSON(http.StatusOK, response{
+		Status:  "success",
+		Message: "pong",
+	})
+}
+
+func addProduct(c echo.Context) error {
+	return nil
+}
+
+func getListProduct(c echo.Context) error {
+	return nil
+}
+
+func getProductByID(c echo.Context) error {
+	return nil
+}
+
+func updateProduct(c echo.Context) error {
+	return nil
+}
+
+func deleteProduct(c echo.Context) error {
+	return nil
+}
+
 func main() {
 	e := echo.New()
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, response{
-			Status:  "success",
-			Message: "pong",
-		})
-	})
+	e.GET("/ping", ping)
+
+	e.POST("/products", addProduct)
+	e.GET("/products", getListProduct)
+	e.GET("/product/:id", getProductByID)
+	e.PUT("/product/:id", updateProduct)
+	e.DELETE("/product/:id", deleteProduct)
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
